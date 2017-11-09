3610632d993ae335edd6e6a52e829317bc84cb32
Grégory Houllier <gregory.houllier@gmail.com>
2017-11-09 15:48:20 +0100

    feat(observer) Add new abstract observer component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
 export { Api, ApiError, createApi } from './src/api'
+export { Observer } from './src/observer'
 export { ZetaPushConnection, ZetaPushClient } from './src/core'
 export { ZetaPushClientConfig } from './src/di'
 export { ZetaPushModule } from './src/zetapush.module'
---
+++ src/lib/src/observer.ts
@@ -0,0 +1,16 @@
+import { OnDestroy } from '@angular/core'
+
+import { Subscription } from 'rxjs/Subscription'
+
+export class Observer implements OnDestroy {
+  protected subscriptions: Subscription[] = []
+
+  observe(subscription: Subscription) {
+    this.subscriptions.push(subscription)
+    return subscription
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe())
+  }
+}
